4f24a80221c66dbc5a58a0bc1bea3e9b984237a2
DheaNurlita <dheanurlitasa23@gmail.com>
2024-08-06 09:34:56 -0700

    chore: add data models

---
+++ lib/dt_domains/_models/a.products.dart
@@ -0,0 +1,100 @@
+part of '_index.dart';
+
+class Products {
+  String name;
+  String id;
+  String createdAt;
+  int price;
+  int quantity;
+  String description;
+  String imageUrl;
+  Products({
+    this.name = '',
+    this.id = '',
+    this.createdAt = '',
+    this.price = 0,
+    this.quantity = 0,
+    this.description = '',
+    this.imageUrl = '',
+  });
+
+  Products copyWith({
+    String? name,
+    String? id,
+    String? createdAt,
+    int? price,
+    int? quantity,
+    String? description,
+    String? imageUrl,
+  }) {
+    return Products(
+      name: name ?? this.name,
+      id: id ?? this.id,
+      createdAt: createdAt ?? this.createdAt,
+      price: price ?? this.price,
+      quantity: quantity ?? this.quantity,
+      description: description ?? this.description,
+      imageUrl: imageUrl ?? this.imageUrl,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    result.addAll({'name': name});
+    result.addAll({'id': id});
+    result.addAll({'created_at': createdAt});
+    result.addAll({'price': price});
+    result.addAll({'quantity': quantity});
+    result.addAll({'description': description});
+    result.addAll({'image_url': imageUrl});
+
+    return result;
+  }
+
+  factory Products.fromMap(Map<String, dynamic> map) {
+    return Products(
+      name: map['name'] ?? '',
+      id: map['id'] ?? '',
+      createdAt: map['created_at'] ?? '',
+      price: map['price']?.toInt() ?? 0,
+      quantity: map['quantity']?.toInt() ?? 0,
+      description: map['description'] ?? '',
+      imageUrl: map['image_url'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Products.fromJson(String source) => Products.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'Products(name: $name, id: $id, createdAt: $createdAt, price: $price, quantity: $quantity, description: $description, imageUrl: $imageUrl)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Products &&
+        other.name == name &&
+        other.id == id &&
+        other.createdAt == createdAt &&
+        other.price == price &&
+        other.quantity == quantity &&
+        other.description == description &&
+        other.imageUrl == imageUrl;
+  }
+
+  @override
+  int get hashCode {
+    return name.hashCode ^
+        id.hashCode ^
+        createdAt.hashCode ^
+        price.hashCode ^
+        quantity.hashCode ^
+        description.hashCode ^
+        imageUrl.hashCode;
+  }
+}
--- lib/xtras/d.serv.dart
+++ lib/xtras/d.serv.dart
@@ -1,5 +1,7 @@
 part of '_index.dart';
 
 abstract class Serv {
+  static AuthServ get auth => AuthServ();
   static SampleServ get sample => SampleServ();
+  static ProductsServ get products => ProductsServ();
 }
