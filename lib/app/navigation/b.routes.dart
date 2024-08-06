c86c1bef4fb11931330ddd7223c8727dbbe47157
DheaNurlita <dheanurlitasa23@gmail.com>
2024-08-06 09:28:00 -0700

    chore: navigation page

--- lib/app/navigation/b.routes.dart
+++ lib/app/navigation/b.routes.dart
@@ -3,11 +3,11 @@ part of '_index.dart';
 class Routes {
   Routes._();
 
-  static const home = '/';
+  static const home = '/product_list';
   static const sampleSatu = '/sample_satu';
   static const sampleDua = '/sample_dua';
   static const sampleTiga = '/sample_tiga';
-  static const productList = '/product_list';
+  static const productList = '/';
 
   static const productAdd = '/product_add';
   static const productDetail = '/product_detail';
