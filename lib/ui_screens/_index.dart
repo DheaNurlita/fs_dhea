afecc21ddc02f710fe7cd5b5870ac7b93517cee1
DheaNurlita <dheanurlitasa23@gmail.com>
2024-08-06 09:23:35 -0700

    chore: add pages

--- lib/ui_screens/_index.dart
+++ lib/ui_screens/_index.dart
@@ -5,3 +5,8 @@ export 'x.sample/a.sample_satu/_index.dart';
 export 'x.sample/b.sample_dua/_index.dart';
 
 export 'x.sample/c.sample_tiga/_index.dart';
+
+export 'a.products/a.product_list/_index.dart';
+export 'a.products/b.product_add/_index.dart';
+export 'a.products/c.product_detail/_index.dart';
+export 'a.products/d.product_edit/_index.dart';
