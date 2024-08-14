f61d4a1aff8cd573061d572d09811304286f6342
DheaNurlita <dheanurlitasa23@gmail.com>
2024-08-14 06:09:03 -0700

    feat: add auth to dt_source

--- lib/dt_sources/_index.dart
+++ lib/dt_sources/_index.dart
@@ -1,8 +1,15 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:fs_dhea/app/_index.dart';
 import 'package:fs_dhea/app/navigation/_index.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:states_rebuilder/scr/state_management/rm.dart';
 
 part 'a.fb_firestore.dart';
 part 'b.fb_storage.dart';
+part 'c.auth.dart';
---
+++ lib/dt_sources/c.auth.dart
@@ -0,0 +1,60 @@
+part of '_index.dart';
+
+final x1fbAuth = RM.inject(
+  () => FbAuth(),
+  debugPrintWhenNotifiedPreMessage: '',
+);
+
+class FbAuth {
+  final instance = FirebaseAuth.instance;
+
+//*----------------------------------------------------------------------------
+
+  Future signInAnonymouse() async {
+    final anonymouse = await instance.signInAnonymously();
+
+    return anonymouse;
+  }
+
+//*----------------------------------------------------------------------------
+
+  Future<UserCredential> signInEmailPassword(String email, String password) async {
+    final user = await instance.signInWithEmailAndPassword(email: email, password: password);
+    return user;
+  }
+
+//*----------------------------------------------------------------------------
+
+  Future logOut() async {
+    final signOut = await instance.signOut();
+
+    return signOut;
+  }
+
+//*----------------------------------------------------------------------------
+
+  Future<UserCredential> signInEmail(String email, String password) async {
+    final user = await instance.signInWithEmailAndPassword(email: email, password: password);
+
+    return user;
+  }
+
+//*----------------------------------------------------------------------------
+
+  Future<UserCredential> signInByGoogle() async {
+    UserCredential? userCredential;
+
+    if (kIsWeb) {
+      final authProvider = GoogleAuthProvider().setCustomParameters({'prompt': 'select_account'});
+      userCredential = await FirebaseAuth.instance.signInWithPopup(authProvider);
+    } else {
+      final googleSignIn = await GoogleSignIn().signIn();
+      final googleAuth = await googleSignIn?.authentication;
+      final googleAuthCredential =
+          GoogleAuthProvider.credential(accessToken: googleAuth?.accessToken, idToken: googleAuth?.idToken);
+
+      userCredential = await FirebaseAuth.instance.signInWithCredential(googleAuthCredential);
+    }
+    return userCredential;
+  }
+}
